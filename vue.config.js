d31a032e71f292bf4eb49f288bce25e4cbeeb46f
Arkadiusz Rypulak <83660682+arkadiuszrypulak@users.noreply.github.com>
2023-05-26 14:25:04 +0200

    .

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/fetch-images/" : "/",
+  base: "/fetch-images/",
   transpileDependencies: true,
 });
